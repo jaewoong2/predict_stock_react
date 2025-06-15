31179eacfd3b4d86522804b1dc7d5ad26ddcc177
jaewoong2 <jwisgenius@naver.com>
2025-06-15 20:39:13 +0900

    Component 수정 및 CORS API 수정

--- src/services/tickerService.ts
+++ src/services/tickerService.ts
@@ -8,8 +8,9 @@ import {
 } from "../types/ticker";
 
 // API 기본 URL 설정
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
+const API_BASE_URL = import.meta.env.DEV
+  ? import.meta.env.VITE_API_LOCAL_URL
+  : import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
 
 // Axios 인스턴스 생성
 const api = axios.create({
