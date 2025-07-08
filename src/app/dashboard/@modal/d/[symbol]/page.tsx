89e558222cd5ce26364f0ec713aacb0fda559f2a
Jaewoong2 <jwisgenius@naver.com>
2025-07-09 07:34:41 +0900

    Modal 구현

--- src/app/dashboard/@modal/d/[symbol]/page.tsx
+++ src/app/dashboard/@modal/d/[symbol]/page.tsx
@@ -5,8 +5,11 @@ import { SignalDetailView } from "@/components/signal/SignalDetailView";
 export default function ModalPage({ params }: { params: { symbol: string } }) {
   const searchParams = useSearchParams();
   const model = searchParams.get("model") ?? "OPENAI";
-  const date = searchParams.get("date") ?? new Date().toISOString().split("T")[0];
+  const date =
+    searchParams.get("date") ?? new Date().toISOString().split("T")[0];
   const router = useRouter();
+
+  console.log("hello world");
   return (
     <SignalDetailView
       open={true}
